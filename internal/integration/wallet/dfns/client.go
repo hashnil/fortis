b252653c6f4fc8259668faeeaa4bc9439c7ec4f1
Swapnil Jaiswal <swapnil@getpanda.money>
2025-02-13 21:11:49 +0530

    DFNS client singleton

--- internal/integration/wallet/dfns/client.go
+++ internal/integration/wallet/dfns/client.go
@@ -7,27 +7,46 @@ import (
 	"fortis/infrastructure/config"
 	"io"
 	"net/http"
+	"sync"
 
 	"github.com/dfns/dfns-sdk-go/credentials"
 	api "github.com/dfns/dfns-sdk-go/dfnsapiclient"
 )
 
-func APIClient[T any](request interface{}, httpMethod, URL string) (*T, error) {
-	// Configure credentials for authentication
-	conf := &credentials.AsymmetricKeySignerConfig{
-		PrivateKey: string(config.DFNS_PRIVATE_KEY), // DFNS Credential Private Key
-		CredID:     config.GetCredentialID(),        // DFNS Credential ID
-	}
+var (
+	dfnsClient    *http.Client
+	dfnsOnce      sync.Once
+	clientInitErr error
+)
 
-	// Create a DFNS API client instance with authentication
-	authToken := config.GetAuthToken()
-	apiOptions, err := api.NewDfnsAPIOptions(&api.DfnsAPIConfig{
-		AppID:     config.GetAppID(),   // DFNS Application ID
-		AuthToken: &authToken,          // Authentication Token
-		BaseURL:   config.GetBaseURL(), // DFNS API Base URL
-	}, credentials.NewAsymmetricKeySigner(conf))
-	if err != nil {
-		return nil, fmt.Errorf("error creating DFNS API options: %w", err)
+func initDFNSClient() {
+	dfnsOnce.Do(func() {
+		// Configure credentials for authentication
+		conf := &credentials.AsymmetricKeySignerConfig{
+			PrivateKey: string(config.DFNS_PRIVATE_KEY), // DFNS Credential Private Key
+			CredID:     config.GetCredentialID(),        // DFNS Credential ID
+		}
+
+		// Create a DFNS API client instance with authentication
+		authToken := config.GetAuthToken()
+		apiOptions, err := api.NewDfnsAPIOptions(&api.DfnsAPIConfig{
+			AppID:     config.GetAppID(),   // DFNS Application ID
+			AuthToken: &authToken,          // Authentication Token
+			BaseURL:   config.GetBaseURL(), // DFNS API Base URL
+		}, credentials.NewAsymmetricKeySigner(conf))
+		if err != nil {
+			clientInitErr = fmt.Errorf("error creating DFNS API options: %w", err)
+		}
+
+		// Create DFNS API client
+		dfnsClient = api.CreateDfnsAPIClient(apiOptions)
+	})
+}
+
+func APIClient[T any](request interface{}, httpMethod, URL string) (*T, error) {
+	initDFNSClient()
+	if clientInitErr != nil {
+		return nil, clientInitErr
 	}
 
 	// Prepare request body if applicable
@@ -41,13 +60,10 @@ func APIClient[T any](request interface{}, httpMethod, URL string) (*T, error) {
 	}
 
 	// Create an HTTP request
-	req, err := http.NewRequest(httpMethod, apiOptions.BaseURL+URL, requestBody)
+	req, err := http.NewRequest(httpMethod, config.GetBaseURL()+URL, requestBody)
 	if err != nil {
 		return nil, fmt.Errorf("error creating HTTP request: %w", err)
 	}
-
-	// Create DFNS API client
-	dfnsClient := api.CreateDfnsAPIClient(apiOptions)
 
 	// Execute the request
 	resp, err := dfnsClient.Do(req)
